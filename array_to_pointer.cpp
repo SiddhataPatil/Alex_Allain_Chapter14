3ccfa9358f08086e55d49c2b7c0635cedb07ccb6
Sid <siddhata.s.patil@gmail.com>
2016-07-14 11:32:49 -0700

    Create array_to_pointer.cpp

---
+++ array_to_pointer.cpp
@@ -0,0 +1,34 @@
+/*
+ ==============================================================================================================================
+ Name        : array_to_pointer.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Assigning array to pointer, printing 1-9, getting the size and address of the first index in an array
+ ==============================================================================================================================
+ */
+ 
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+
+int A[10];
+cout<<"Array size"<<sizeof(A)<<endl;
+int *p = A;
+
+for (int i=0; i<10; i++)
+{
+	p[i] = i+1;
+	cout <<p[i]<< "  " ;
+}
+cout << "Size now: "<<sizeof(p)<<endl;
+cout<< *p<< " address->" <<p<<endl;
+
+
+	system ("pause");
+	return 0;
+}
