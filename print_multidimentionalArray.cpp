201dffced77de605d61c5fac975edb6df7e7f185
Sid <siddhata.s.patil@gmail.com>
2016-07-14 17:32:54 -0700

    Update print_multidimentionalArray.cpp

--- print_multidimentionalArray.cpp
+++ print_multidimentionalArray.cpp
@@ -9,7 +9,27 @@
  */
  
 #include <iostream>
-#include <string>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
+
+void print_array(int A[][3], int row)
+{
+	for (int i =0; i<row; i++)
+	{
+		for(int j=0; j< 3; j++)
+		{
+			//cout << &A[i][j]<< "  ";
+			cout << A[i][j]<< "  ";
+		}
+		cout<<endl;
+	}
+}
+
+int main()
+{
+	int mult_array[2][3]= {{1,2,3},{4,5,6}}; // ***
+											                               // ***
+	print_array(mult_array, 2);
+
+	system ("pause");
+	return 0;
+}
