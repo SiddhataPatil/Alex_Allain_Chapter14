6ee2fd69c39b9c531d7829483e75871e89783c38
Sid <siddhata.s.patil@gmail.com>
2016-07-15 10:33:09 -0700

    Create qs4.cpp

---
+++ qs4.cpp
@@ -0,0 +1,103 @@
+/*
+ ==============================================================================================================================
+ Name        : qs4.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    :Write a program that lets users keep track of the last time they talked to each of their friends.
+              Users should be able to add new friends (as many as they want!) and store the number of days
+              ago that they last talked to each friend. Let users update this value (but don't let them put in
+              bogus numbers like negative values). Make it possible to display the list sorted by the names of
+              the friends of by how recently it was since they talked to each friend.
+ ==============================================================================================================================
+ */
+ 
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void print_ascending(string* p, int* x,int size) // has to be corrected
+{
+	//int* a = new int [size];
+	int j,k,s;
+	string temp=" ";
+	for(s=0; s<size/2; s++)
+	{
+	for ( j=0; j<size-1; j++)
+	{
+		if(x[j]<x[j+1])
+		{
+		temp = p[j];
+		p[j]=p[j+1];
+		p[j+1] = temp;
+		}
+	}
+	}
+	for ( k=0; k<size; k++)
+	{
+		cout<< p[k]<<endl;
+	}
+}
+void Info(string* p, int* x, int size)
+{int max=0;
+for ( int k=0; k<size; k++)
+	{
+		if (x[k] > x[max])
+			max=k;
+	}
+cout<< "Most called friend is "<<p[max]<< endl;
+
+}
+struct callers
+{
+	string name;
+	int no;
+};
+callers blah;
+int main()
+
+{	
+	string* p= new string[20];
+	int i,t;
+	const int row_size=20;
+	int size;
+	int loop_condition=0;
+	int* x = new int [row_size];
+	
+cout<< "Enter no of friends you want to keep in contact"<< endl;
+	cin>>size;
+int count;
+		for ( i=0; i<size; i++)
+	{
+		cout<<"Enter name and no of times called: "<<i+1<<" ] "<<endl;
+		cin>>blah.name;
+		p[i]= blah.name;
+		cin>>blah.no;
+		x[i] = blah.no;
+	}
+		while(loop_condition<10)
+	{
+		t=i;
+	Info(p,x, size);
+	print_ascending(p,x,size);
+	cout<<"How many more friends you want to add"<<endl;
+	cin>> count;
+	size= size+count;
+for ( i=t; i<size; i++)
+	{
+		cout<<"Enter name and no of times called: "<<i+1<<" ] "<<endl;
+		cin>>blah.name;
+		p[i]= blah.name;
+		cin>>blah.no;
+		x[i] = blah.no;
+	} continue;
+	loop_condition++;
+	}
+	delete[] x;
+	delete x;
+	
+	system ("pause");
+	return 0;
+}
